5ead5e44cf6d304873a49192cbd12a220348a70b
João Bernardo Dias de Brito <Drip.designer16@hotmail.com>
2025-09-05 15:50:39 -0300

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,18 @@
+﻿using AppGerenciadorDeTarefas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGerenciadorDeTarefas.Usuário
+{
+    public class Usuario
+    {
+        public string nome;
+        public string email;
+        public string senha;
+        public List<Tarefa> tarefas;
+
+    }
+}
