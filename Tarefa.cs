850bfe7c9154bf4ae8ae3678714e32953b966cf1
João Bernardo Dias de Brito <Drip.designer16@hotmail.com>
2025-09-05 15:50:19 -0300

    Add files via upload

---
+++ Tarefa.cs
@@ -0,0 +1,20 @@
+﻿using AppGerenciadorDeTarefas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGerenciadorDeTarefas.Model
+{
+    public class Tarefa
+    {
+        public string descricao;
+        public bool concluida;
+        public Tarefa(string descricao)
+        {
+            this.descricao = descricao;
+            concluida = false;
+        }
+    }
+}
